7198f1186fccc3f3055dce7e1f46d543d2affae8
paramsandhu2103 <113151379+paramsandhu2103@users.noreply.github.com>
2024-06-14 22:44:48 -0400

    Added

---
+++ controllers/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+// Define routes
+router.get('/', (req, res) => {
+    res.render('home', { title: 'Home' });
+});
+
+router.get('/about', (req, res) => {
+    res.render('about', { title: 'About Me' });
+});
+
+router.get('/projects', (req, res) => {
+    res.render('projects', { title: 'Projects' });
+});
+
+router.get('/contact', (req, res) => {
+    res.render('contact', { title: 'Contact Me' });
+});
+
+module.exports = router;
