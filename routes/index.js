1ca64617f07bd1563462bf2ae93979a619038fee
paramsandhu2103 <113151379+paramsandhu2103@users.noreply.github.com>
2024-06-14 23:47:01 -0400

    complete

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
+// GET home page
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// GET About Me page
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me' });
+});
+
+// GET Projects page
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+
+// GET Contact Me page
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me' });
 });
 
 module.exports = router;
